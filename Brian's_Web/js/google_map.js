ef6e53741642df161d988993fda9de2a04f5f7f7
102213047 <s102213047@mail1.ncnu.edu.tw>
2017-04-17 12:45:41 +0800

    Revise

---
+++ Brian's_Web/js/google_map.js
@@ -0,0 +1,14 @@
+<script>
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+</script>
+
+<!--
+To use this code on your website, get a free API key from Google.
+Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+-->
